4e4d498bf8520338fe83c68fde58f4b431f8c8c9
Dishant Arora <dishantarora16@gmail.com>
2023-09-11 21:39:19 +0530

    added caricature

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,11 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 const Home = () => {
   return (
     <LinearGradient colors={["#090945", "#db5dce"]} style={styles.container}>
       <View>
-        <Text></Text>
+        <Image style={styles.cari} source={require('../assets/jalshi-cari.png')}/>
       </View>
     </LinearGradient>
   );
@@ -17,4 +17,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  cari: {
+    width: 200,
+    height: 250,
+    position: 'absolute',
+    top: 420,
+    left: 225,
+  }
 });
